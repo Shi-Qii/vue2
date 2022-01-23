e7eb1bfeb90d3a1a2b9593294bb1353a024ca8df
Shi-Qi <qweqwe0528@gmail.com>
2022-01-23 19:24:22 +0800

    新增 修改 全端串接

--- src/components/test123564.vue
+++ src/components/test123564.vue
@@ -12,29 +12,66 @@
 
 <script>
 import GetStockData from '../services/getStockData';
+import VueCompositionAPI, {onMounted, ref} from "@vue/composition-api";
+import Vue from 'vue'
 
+Vue.use(VueCompositionAPI)
 export default {
-  mounted() {
-    GetStockData.getUserBoard().then(res => {
-      console.log('res', res.data)
-      this.items=res.data;
-    })
-  },
-  data() {
-    return {
-      items: [
+  setup() {
+    const items = ref([]);
+    onMounted(() => {
+      GetStockData.getUserBoard().then(res => {
+        const stockData = res.data;
+        // this.items=res.data;
 
-      ],
-      fields1: [
-        {key: 'dealer', label: 'ID'},
-        {key: 'foreign_investors', label: 'Type 1'},
-        {key: 'investment_trust', label: 'Type 2A'},
-        {key: 'market_type', label: 'Type 2B'},
-        {key: 'processing_date', label: 'Type 2C'},
-        {key: 'stock_num', label: 'Type 3'},
-        {key: 'total_buysell', label: 'Type 3'}
-      ]
+        console.log(stockData)
+        // const data = res.data;
+
+        let obj = {
+          Processing_date: null,
+          Stock_num: null,
+          Stock_name: null,
+          Foreign_investors: null,
+          Investment_trust: null,
+          Dealer: null,
+          Total_buysell: null,
+        }
+
+        for (let i = 0; i < stockData.Dealer.length; i++) {
+          obj.Processing_date = stockData.Processing_date[i]
+          obj.Stock_num = stockData.Stock_num[i]
+          obj.Stock_name = stockData.Stock_name[i]
+          obj.Foreign_investors = stockData.Foreign_investors[i]
+          obj.Investment_trust = stockData.Investment_trust[i]
+          obj.Dealer = stockData.Dealer[i]
+          obj.Total_buysell = stockData.Total_buysell[i]
+          items.value.push(obj)
+          fields1.value.push(
+
+              {key: 'Stock_num', label: '公司代號'},
+              {key: 'Stock_name', label: '股票名稱'},
+              {key: 'Dealer', label: '自營買賣超張數'},
+              {key: 'Foreign_investors', label: '外資買賣超張數'},
+              {key: 'Investment_trust', label: '投資買賣超張數'},
+              {key: 'Processing_date', label: 'Type 1'},
+              {key: 'Total_buysell', label: '總買賣超張數'})
+
+          console.log('items:', items)
+        }
+
+      })
+
+
+    })
+    const test1 = ref('123')
+
+
+    const fields1 = ref([]);
+
+    return {
+      test1, items, fields1
     }
   }
+
 }
 </script>
