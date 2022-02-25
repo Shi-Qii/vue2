216752f3136c30aad208b08c6b0263e59bfcca0a
Shi-Qi <qweqwe0528@gmail.com>
2022-02-25 23:54:16 +0800

    new monthlyrevenue.vue

--- src/components/chartFolder/monthlyChart.vue
+++ src/components/chartFolder/monthlyChart.vue
@@ -109,7 +109,7 @@ export default {
         name: '每月營收',
         type: 'column',
         yAxis: 1,
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        data: [],
         tooltip: {
           valueSuffix: ' '
         },
@@ -117,7 +117,7 @@ export default {
       }, {
         name: '月均價',
         type: 'spline',
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+        data: [],
         tooltip: {
           valueSuffix: ' '
         },
