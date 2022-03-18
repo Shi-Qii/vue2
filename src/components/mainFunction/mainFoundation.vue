b23d7c98972016423083bec396e1bd1f32538e39
Shi-Qi <qweqwe0528@gmail.com>
2022-03-18 22:04:36 +0800

    把法人抽出 mainFoundation.vue 後面塞入參數

--- src/components/mainFunction/mainFoundation.vue
+++ src/components/mainFunction/mainFoundation.vue
@@ -334,6 +334,7 @@ export default {
       editHTMLcolorClassification: () => {
         individualVueData.items.value.forEach((f) => {
           console.log('變更table設定')
+          Math.round(f['Up_down_pct']);
           let Updownpct = f['Up_down_pct']; //漲跌幅
           /*
               * 對應漲跌  + 紅色
@@ -342,8 +343,14 @@ export default {
           if (Updownpct > 9.5) {
             f['_cellVariants'] = {Up_down_pct: 'danger'}
           }
-          if ('外資+自營買超' === props['params']['foreignNm']) {
+          if ('外資+投信買超' === props['params']['foreignNm']) {
             f['add_Up'] = f['Dealer'] + f['Foreign_investors'];
+          }
+          if ('外資+自營買超' === props['params']['foreignNm']) {
+            f['add_Up'] = f['Investment_Trust'] + f['Foreign_investors'];
+          }
+          if ('自營+投信買超' === props['params']['foreignNm']) {
+            f['add_Up'] = f['Dealer'] + f['Investment_Trust'];
           }
         })
       },
