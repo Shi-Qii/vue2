c3024781f9b474287ba4dcdf3882c82e9edf2033
SHIQI <qweqwe0528@gmail.com>
2022-05-04 22:15:43 +0800

    test

--- src/components/featuresVue/dividendQuery.vue
+++ src/components/featuresVue/dividendQuery.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
 
-    <div class="container-fluid">
+    <div class="container-fluid ">
+      <p>test git </p>
       <!--      <dividendQueryColunmChart v-if="showState.showTable" :initChartData="initChartData"></dividendQueryColunmChart>-->
       <dividend-query-colunm-chart :initChartData="initChartData"></dividend-query-colunm-chart>
       <b-table
