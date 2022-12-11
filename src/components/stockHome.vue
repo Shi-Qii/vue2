21306e46c731539e959b5178e105804c61ccc061
SHIQI <qweqwe0528@gmail.com>
2022-12-11 22:24:52 +0800

    stockHome.vue edit

--- src/components/stockHome.vue
+++ src/components/stockHome.vue
@@ -1,77 +1,28 @@
 <template>
   <div>
-
-
-    {{'Welcome'}}
-<!--    <b-spinner label="Spinning"></b-spinner>-->
-
+    {{ 'Welcome' }}
   </div>
 </template>
-
 <script>
-// import GetStockData from '../services/getStockData';
-import VueCompositionAPI, { ref} from "@vue/composition-api";
+import VueCompositionAPI, {ref, onMounted} from "@vue/composition-api";
 import Vue from 'vue'
-
+import axios from 'axios';
 Vue.use(VueCompositionAPI)
 export default {
   setup() {
-    const items = ref([]);
-    const showSidebar=ref(true)
-    // onMounted(() => {
-    //   GetStockData.getUserBoard().then(res => {
-    //     const stockData = res.data;
-    //     // this.items=res.data;
-    //
-    //     console.log(stockData)
-    //     // const data = res.data;
-    //
-    //
-    //
-    //     for (let i = 0; i < stockData.Dealer.length; i++) {
-    //       let obj = {
-    //         Processing_date: null,
-    //         Stock_num: null,
-    //         Stock_name: null,
-    //         Foreign_investors: null,
-    //         Investment_trust: null,
-    //         Dealer: null,
-    //         Total_buysell: null,
-    //       }
-    //       obj.Processing_date = stockData.Processing_date[i]
-    //       obj.Stock_num = stockData.Stock_num[i]
-    //       obj.Stock_name = stockData.Stock_name[i]
-    //       obj.Foreign_investors = stockData.Foreign_investors[i]
-    //       obj.Investment_trust = stockData.Investment_trust[i]
-    //       obj.Dealer = stockData.Dealer[i]
-    //       obj.Total_buysell = stockData.Total_buysell[i]
-    //       items.value.push(obj)
-    //       fields1.value.push(
-    //
-    //           {key: 'Stock_num', label: '公司代號'},
-    //           {key: 'Stock_name', label: '股票名稱'},
-    //           {key: 'Dealer', label: '自營買賣超張數'},
-    //           {key: 'Foreign_investors', label: '外資買賣超張數'},
-    //           {key: 'Investment_trust', label: '投信買賣超張數'},
-    //           {key: 'Processing_date', label: '日期'},
-    //           {key: 'Total_buysell', label: '總買賣超張數'})
-    //
-    //       console.log('items:', items)
-    //     }
-    //
-    //   })
-    //
-    //
-    // })
+    const items = ref();
+    const showSidebar = ref(true)
+    onMounted(() => {
+      const API_URL = 'http://localhost:8070/api/test';
+      axios.get(API_URL).then((res) => {
+        items.value = res.data;
+      })
+    })
     const test1 = ref('123')
-
-
     const fields1 = ref([]);
-
     return {
-      test1, items, fields1,showSidebar
+      test1, items, fields1, showSidebar
     }
   }
-
 }
 </script>
