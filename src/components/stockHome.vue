f8a52a1ed61253df0b16461c39f0f53a83d16966
SHIQI <qweqwe0528@gmail.com>
2022-12-11 23:06:55 +0800

    stockHome.vue edit

--- src/components/stockHome.vue
+++ src/components/stockHome.vue
@@ -14,7 +14,7 @@ export default {
     const showSidebar = ref(true)
     onMounted(() => {
       // http://45.32.108.38:8070/api/test
-      const API_URL = 'http://45.32.108.38:8070/api/test';
+      const API_URL = 'http://45.32.108.38:8071/api/test';
       axios.get(API_URL).then((res) => {
         items.value = res.data;
       })
